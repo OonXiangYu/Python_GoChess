8bd35685cdf7e2e69f8b77a7687286f535093390
OonXiangYu <oonxy-wm22@student.tarc.edu.my>
2024-12-16 16:26:02 +0000

    version 2.0
    can place the pieces

--- Python_GoChess/FirstName_LastName_StudentNumber_Project/code/board.py
+++ Python_GoChess/FirstName_LastName_StudentNumber_Project/code/board.py
@@ -4,7 +4,7 @@ from PyQt6.QtGui import QPainter, QColor, QBrush
 
 class Board(QFrame):  # base the board on a QFrame widget
     updateTimerSignal = pyqtSignal(int)  # signal sent when the timer is updated
-    clickLocationSignal = pyqtSignal(str)  # signal sent when there is a new click location
+    clickLocationSignal = pyqtSignal(QPoint)  # signal sent when there is a new click location
 
     # TODO set the board width and height to be square
     boardWidth = 7  # board is 0 squares wide # TODO this needs updating
@@ -24,17 +24,13 @@ class Board(QFrame):  # base the board on a QFrame widget
         self.isStarted = False  # game is not currently started
         self.start()  # start the game which will start the timer
 
-        self.boardArray = [[0 for _ in range(7)] for _ in range(7)]    # TODO - create a 2d int/Piece array to store the state of the game
+        self.boardArray = [[0 for _ in range(8)] for _ in range(8)]    # TODO - create a 2d int/Piece array to store the state of the game
         self.printBoardArray()    # TODO - uncomment this method after creating the array above
 
     def printBoardArray(self):
         '''prints the boardArray in an attractive way'''
         print("boardArray:")
         print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in self.boardArray]))
-
-    def mousePosToColRow(self, event):
-        '''convert the mouse click event to a row and column'''
-        pass  # Implement this method according to your logic
 
     def squareWidth(self):
         '''Returns the width of one square in the game board considering the margins'''
@@ -62,18 +58,32 @@ class Board(QFrame):  # base the board on a QFrame widget
 
     def paintEvent(self, event):
         '''paints the board and the pieces of the game'''
-        print("paint trigger")
         painter = QPainter(self)
         self.drawBoardSquares(painter)
-        print("done paint")
-        #self.drawPieces(painter)
+
+        self.drawPieces(painter)
+
+    def mousePosToColRow(self, click_point: QPoint):
+        '''convert the mouse click event to a row and column'''
+        cell_width = self.width() / 8
+        cell_height = self.height() / 8
+
+        col = int(click_point.x() / cell_width)
+        row = int(click_point.y() / cell_height)
+
+        return row,col
 
     def mousePressEvent(self, event):
         '''this event is automatically called when the mouse is pressed'''
-        clickLoc = "click location [" + str(event.x()) + "," + str(event.y()) + "]"  # the location where a mouse click was registered
-        print("mousePressEvent() - " + clickLoc)
-        # TODO you could call some game logic here
-        self.clickLocationSignal.emit(clickLoc)
+        clickLoc = event.position().toPoint()  # get the click position
+        #print(f"Mouse click at: {clickLoc}")  # Convert QPoint for debugging
+        row, col = self.mousePosToColRow(clickLoc)
+        #self.clickLocationSignal.emit(clickLoc)  # Emit signal
+
+        # Save the clicked point and trigger a repaint
+        self.boardArray[row][col] = 1
+        self.printBoardArray()
+        self.update()
 
     def resetGame(self):
         '''clears pieces from the board'''
@@ -109,13 +119,31 @@ class Board(QFrame):  # base the board on a QFrame widget
 
     def drawPieces(self, painter):
         '''draw the pieces on the board'''
+        '''
         for row in range(0, len(self.boardArray)):
             for col in range(0, len(self.boardArray[0])):
                 painter.save()
                 painter.translate(col * self.squareWidth(), row * self.squareHeight())
                 # TODO draw some pieces as ellipses
                 # TODO choose your color and set the painter brush to the correct color
+                painter.setBrush(QBrush(QColor(0, 0, 255)))  # Set the circle's color to blue
+
                 radius = (self.squareWidth() - 2) / 2
                 center = QPoint(radius, radius)
                 painter.drawEllipse(center, radius, radius)
                 painter.restore()
+        '''
+        cell_width = self.width() / 8
+        cell_height = self.height() / 8
+        painter.setBrush(QBrush(QColor(255, 0, 0)))  # Red color
+        try:
+            for row_idx, row in enumerate(self.boardArray): # based on board array i will know which position i need to place a piece
+                for col_idx, point in enumerate(row):
+                    if point != 0:
+                        center_x = int((col_idx + 0.55) * cell_width)
+                        center_y = int((row_idx + 0.45) * cell_height)
+                        radius = int(min(cell_width, cell_height) / 4)
+                        painter.drawEllipse(center_x - radius, center_y - radius, radius * 2, radius * 2)
+        except Exception as e:
+            print(e)
+
--- Python_GoChess/FirstName_LastName_StudentNumber_Project/code/piece.py
+++ Python_GoChess/FirstName_LastName_StudentNumber_Project/code/piece.py
@@ -1,4 +1,5 @@
 class Piece(object):
+
     NoPiece = 0
     White = 1
     Black = 2
