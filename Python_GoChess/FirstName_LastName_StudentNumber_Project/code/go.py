b9d2d991a8930a0adff4faa848c452fb8693d270
OonXiangYu <oonxy-wm22@student.tarc.edu.my>
2024-12-27 13:40:33 +0000

    version 5.3
    - adding rules display
    
    Task
    enhance the UI and Design

--- Python_GoChess/FirstName_LastName_StudentNumber_Project/code/go.py
+++ Python_GoChess/FirstName_LastName_StudentNumber_Project/code/go.py
@@ -1,5 +1,6 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow, QDockWidget, QMenuBar
+from PyQt6.QtWidgets import QApplication, QMainWindow, QDockWidget, QMenuBar, QMessageBox
 from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QAction
 
 from board import Board
 from score_board import ScoreBoard
@@ -23,6 +24,12 @@ class Go(QMainWindow):
         self.board = Board(self)
         self.setCentralWidget(self.board)
 
+        menuBar = self.menuBar()
+        rules = menuBar.addMenu("Rules")
+        ruleAction = QAction("Rules",self)
+        ruleAction.triggered.connect(self.showRule)
+        rules.addAction(ruleAction)
+
         self.scoreBoard = ScoreBoard()
         self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.scoreBoard)
         self.scoreBoard.make_connection(self.board)
@@ -41,6 +48,16 @@ class Go(QMainWindow):
         x = (screen.width() - size.width()) // 2
         y = (screen.height() - size.height()) // 2
         self.move(x, y)
+
+    def showRule(self): # window to show rules
+        msg_box = QMessageBox(self)
+        msg_box.setWindowTitle("Rule")
+        msg_box.setText("1. Every Players have only 2 mins for play \n"
+                        "2. Players can't redo continuously \n"
+                        "3. Player who reach above 32 territory first victory \n"
+                        "4. Players have unlimited pass chances")
+        msg_box.setIcon(QMessageBox.Icon.Information)
+        msg_box.exec()
 
 
 
